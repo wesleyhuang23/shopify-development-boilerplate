ca5635c279420bfd47be4dca3601233d2ab66828
Anders Søgaard <andershagbard@gmail.com>
2020-02-28 12:28:50 +0100

    Add Purge CSS

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,14 @@ const plugins = [
 ];
 
 if (process.env.NODE_ENV === 'production') {
+  plugins.push(require('@fullhuman/postcss-purgecss')({
+    content: [
+      './resources/html-scrape/*.html',
+      './resources/js/**/*.js',
+      './src/**/*.liquid',
+    ],
+  }));
+
   plugins.push(require('cssnano'));
 
   plugins.push(require('postcss-discard-comments')({
